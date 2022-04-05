45f57e71b577436fc4c1f01725bbc740b0547070
Jeffy Fung <jeffyfungkh@gmail.com>
2022-04-06 01:27:23 +0800

    2.11 the phonebook step6

--- exercise/part2/2.6_phonebook/src/App.js
+++ exercise/part2/2.6_phonebook/src/App.js
@@ -1,12 +1,18 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Filter from './Filter'
 import PersonForm from './PersonForm'
+import axios from 'axios'
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [searchText, setSearchText] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+      .then(response => setPersons(response.data));
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault();
